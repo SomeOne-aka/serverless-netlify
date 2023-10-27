b29e712e4e01508467bba010303b6f31d7641970
SomeOne-aka <tred63957cs@gmail.com>
2023-10-27 10:24:50 +0100

    serverless functions debug

--- serverless/jokes.js
+++ serverless/jokes.js
@@ -1,18 +1,15 @@
-export async function handler(event, context) {
-  const fetch = await import("node-fetch");
+const fetch = require("node-fetch");
 
+exports.handler = async (event, context) => {
   const url = "https://icanhazdadjoke.com/";
-
   try {
-    const response = await fetch.default(url, {
+    const response = await fetch(url, {
       method: "GET",
       headers: {
         Accept: "application/json",
       },
     });
-
     const jsonJoke = await response.json();
-
     return {
       statusCode: 200,
       body: JSON.stringify(jsonJoke),
@@ -20,4 +17,4 @@ export async function handler(event, context) {
   } catch (error) {
     return { statusCode: 422, body: error.stack };
   }
-}
+};
